0a364a6ce4836ef447a4eb5997a7e8600950cc6e
ninosroger <ninos_r@foxmail.com>
2016-02-24 14:20:46 +0800

    孙正 2016年2月24日14:20:38

--- app/src/main/java/com/hewuzhe/banner/ImagePagerAdapter.java
+++ app/src/main/java/com/hewuzhe/banner/ImagePagerAdapter.java
@@ -61,7 +61,7 @@ public class ImagePagerAdapter extends BaseAdapter {
 		imageLoader = ImageLoader.getInstance();
 		imageLoader.init(ImageLoaderConfiguration.createDefault(context));
 		options = new DisplayImageOptions.Builder()
-				.showStubImage(R.drawable.downing) // 设置图片下载期间显示的图片
+				.showStubImage(R.drawable.nopic) // 设置图片下载期间显示的图片
 				.showImageForEmptyUri(R.drawable.nopic) // 设置图片Uri为空或是错误的时候显示的图片
 				.showImageOnFail(R.drawable.nopic) // 设置图片加载或解码过程中发生错误显示的图片
 				.cacheInMemory(true) // 设置下载的图片是否缓存在内存中
@@ -85,7 +85,7 @@ public class ImagePagerAdapter extends BaseAdapter {
 		imageLoader = ImageLoader.getInstance();
 		imageLoader.init(ImageLoaderConfiguration.createDefault(context));
 		options = new DisplayImageOptions.Builder()
-				.showStubImage(R.drawable.downing) // 设置图片下载期间显示的图片
+				.showStubImage(R.drawable.nopic) // 设置图片下载期间显示的图片
 				.showImageForEmptyUri(R.drawable.nopic) // 设置图片Uri为空或是错误的时候显示的图片
 				.showImageOnFail(R.drawable.nopic) // 设置图片加载或解码过程中发生错误显示的图片
 				.cacheInMemory(true) // 设置下载的图片是否缓存在内存中
